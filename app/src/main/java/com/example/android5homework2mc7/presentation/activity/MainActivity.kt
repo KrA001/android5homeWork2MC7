51996320aa9f97fc87f5fa3af89e6e421e620f22
LENOVO <test23062000@gmail.com>
2024-10-04 23:38:31 +0300

    дз с допом и сокращение

--- app/src/main/java/com/example/android5homework2mc7/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/android5homework2mc7/presentation/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android5homework2mc7.presentation.activity
 
+import FirstScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,19 +11,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android5homework2mc7.R
-import com.example.jetpackcompose.FirstScreen
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
+        super.onCreate(savedInstanceState)  // Вызывает метод родительского класса
+        enableEdgeToEdge() // Включает режим "от края до края" для экрана
+        setContent { // Устанавливает содержимое экрана
             SplashScreenApp()
         }
     }
@@ -30,41 +29,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SplashScreenApp() {
-    var showSplash by remember { mutableStateOf(true) }
+    var showSplash by remember { mutableStateOf(true) } // Состояние для отображения SplashScreen
 
     // Показываем SplashScreen на 3 секунды
-    LaunchedEffect(Unit) {
-        delay(3000)
-        showSplash = false
+    LaunchedEffect(Unit) { // Эффект запускается при первой компоновке
+        delay(3000) // Задержка на 3 секунды
+        showSplash = false // Скрывает SplashScreen
     }
 
-    if (showSplash) {
-        // SplashScreen
-        SplashScreen()
-    } else {
-        // Основной экран после SplashScreen
-       FirstScreen()
-    }
+    // Условное отображение экрана
+    if (showSplash) SplashScreen() else FirstScreen() // Отображение SplashScreen или основного экрана
 }
 
 @Composable
 fun SplashScreen() {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Brush.verticalGradient(listOf(Color(0xFF000000), Color(0xFF000000)))),
-        contentAlignment = Alignment.Center
+            .fillMaxSize() // Заполняет всю доступную область
+            .background(Color.Black), // Устанавливает черный фон
+        contentAlignment = Alignment.Center // Центрирует содержимое по центру
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_travel_logo),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_travel_logo), // Загружает изображение из ресурсов
+            contentDescription = null, // Описание изображения для доступности
+            modifier = Modifier.fillMaxSize() // Заполняет всю область изображения
+        )
     }
 }
-
 
 @Preview
 @Composable
